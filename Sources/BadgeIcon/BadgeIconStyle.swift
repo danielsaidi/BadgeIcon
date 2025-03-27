81773e7f05002cf04c2f4d1f6eb6927bffeb6626
Daniel Saidi <daniel.saidi@gmail.com>
2025-03-27 15:57:37 +0100

    Update copyright

--- Sources/BadgeIcon/BadgeIconStyle.swift
+++ Sources/BadgeIcon/BadgeIconStyle.swift
@@ -3,7 +3,7 @@
 //  BadgeIcon
 //
 //  Created by Daniel Saidi on 2024-04-29.
-//  Copyright © 2024 Daniel Saidi. All rights reserved.
+//  Copyright © 2024-2025 Daniel Saidi. All rights reserved.
 //
 
 import SwiftUI
